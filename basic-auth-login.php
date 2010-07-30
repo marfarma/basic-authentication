7d1815dbbada4b32c1a05792219473cb486d931d
cuve <cuve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-30 19:14:59 +0000

    typo fix
    git-svn-id: http://svn.wp-plugins.org/basic-authentication/trunk@269499 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- basic-auth-login.php
+++ basic-auth-login.php
@@ -10,7 +10,7 @@
 	 */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
-	<head>æ
+	<head>
 		<title><?php echo get_option('blogname'); ?> &rsaquo; Log In</title>
 	
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
